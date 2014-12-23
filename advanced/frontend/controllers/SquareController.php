ce56a5c7ba82d566a781efcd3b8d1701baa18fe1
zhang <1101010@qq.com>
2014-12-23 16:34:46 +0800

    ss

--- advanced/frontend/controllers/SquareController.php
+++ advanced/frontend/controllers/SquareController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace frontend\controllers;
-
+use app\models\KekeWitkeyWeibo;
+use yii\data\Pagination;
+use yii\data\ActiveDataProvider;
 class SquareController extends \yii\web\Controller
 {
     public function actionSquare(){	
@@ -10,7 +12,7 @@ class SquareController extends \yii\web\Controller
     	$count = clone $list;
     	$pages = new Pagination(['totalCount' =>$count->count(), 'pageSize' => '20']);
         $data['list'] = $list->offset($pages->offset)->where('1=1')->limit($pages->limit)->all();      
-        return $this->render('index',array("data"=>$data,"pages"=>$pages));
+        return $this->render('square',array("data"=>$data,"pages"=>$pages));
     }
 
 }
